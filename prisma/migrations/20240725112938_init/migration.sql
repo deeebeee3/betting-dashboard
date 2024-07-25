ed722eaea9c1b31f702bf437ddd6c9ba407ae4cf
Deepak Bhari <deeebeee3@hotmail.com>
2024-07-25 17:31:28 +0100

    update readme

--- prisma/migrations/20240725112938_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Event" (
-    "event_id" SERIAL NOT NULL,
-    "event_name" TEXT NOT NULL,
-    "odds" DOUBLE PRECISION NOT NULL,
-
-    CONSTRAINT "Event_pkey" PRIMARY KEY ("event_id")
-);
